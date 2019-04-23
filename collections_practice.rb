b153a7185d51ccedd7974871a045bba655f213d5
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-23 21:57:59 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      -1
+    elsif a > b
+      1
+    end
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    if a == b
+      0
+    elsif a < b
+     1
+    elsif a > b
+     -1
+    end
+  end 
+ end
+ 
+ def sort_array_char_count(array)
+   array.sort_by {|x| x.length}
+ end
+ 
+   
+   
+   
+   
+   
+   
+   
+   
+   
